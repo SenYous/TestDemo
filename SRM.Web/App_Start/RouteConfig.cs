654f3f4c37bb28a491ae600172b7e009a67d9c10
Suren_CK <surenck7246@qq.com>
2020-01-16 14:45:18 +0800

    20200116ck1_update

--- SRM.Web/App_Start/RouteConfig.cs
+++ SRM.Web/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace SRM.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "HomeCk", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ SRM.Web/Controllers/HomeCkController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SRM.Web.Controllers
+{
+    public class HomeCkController : Controller
+    {
+        // GET: HomeCk
+        public ViewResult Index()
+        {
+            return View();
+        }
+    }
+}
